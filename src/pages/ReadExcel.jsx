ba8abc32487d5c8c8fa4049e040b55efc5985c83
zhuchuanlei <zhuchuanlei@163.com>
2022-06-23 22:27:17 +0800

    Excel下载

--- src/pages/ReadExcel.jsx
+++ src/pages/ReadExcel.jsx
@@ -2,57 +2,46 @@ import React, { useState } from 'react';
 import { Upload, Button, message } from 'antd';
 import * as XLSX from 'xlsx';
 
-const ReadExcel = () => {
-  const [fileList, setFileList] = useState([]);
+const data = [];
+
+const ReadExcel = (props) => {
   const onImportExcel = (file) => {
-    setFileList([file])
-    // 获取上传的文件对象
-    let data = [];
-
-    let error = [];
-
     let output_data = undefined;
-
     const fileReader = new FileReader();
-
     fileReader.readAsBinaryString(file);
-
     fileReader.onload = event => {
       const { result } = event.target;
-      // 以二进制流方式读取得到整份excel表格对象
       const workbook = XLSX.read(result, { type: 'binary' });
-      // 存储获取到的数据
+      const sheet = workbook.Sheets['顾客购买表']
+      output_data = XLSX.utils.sheet_to_json(sheet, { defval: '' });
+      data.push(output_data)
 
-      // 遍历每张工作表进行读取（这里默认只读取第一张表）
-      for (const sheet in workbook.Sheets) {
-        // esline-disable-next-line
-        if (workbook.Sheets.hasOwnProperty(sheet)) {
-          // 利用 sheet_to_json 方法将 excel 转成 json 数据
-          output_data = data.concat(XLSX.utils.sheet_to_json(workbook.Sheets[sheet], { defval: '' }));
-          // break; // 如果只取第一张表，就取消注释这行
-        }
-      }
-      console.log(output_data);
-      // 最终获取到并且格式化后的 json 数据
-      output_data.forEach(ele => {
-        Object.keys(ele).forEach(val => {
-          if (val.indexOf('EMPTY') === -1) {
-            if (ele[val] === '') {
-              error.push(`第${ele.__rowNum__}行:${val}为空`)
-            }
-          } else {
-            delete ele[val]
-          }
-        })
-      })
     };
+  }
+  const onChange = (e) => {
+    const noSuccess = e.fileList.find(i => i.status !== "done");
+    if (!noSuccess) {
+      props.onChangeData(data);
+    }
+  }
+  const onClickClear = () => {
+    window.location.reload()
   }
 
   return (
-    <Upload fileList={fileList} beforeUpload={onImportExcel}>
+    <>
+    
+    <Upload
+      maxCount={10}
+      multiple={true}
+      beforeUpload={onImportExcel}
+      onChange={onChange}
+    >
       <Button type='primary'>上传</Button>
     </Upload>
 
+<Button onClick={onClickClear}>清除数据</Button>
+</>
   );
 }
 export default ReadExcel;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,70 @@
-import react from 'react';
+import react, { useState } from 'react';
 import { Upload, Button, message } from 'antd';
 import * as XLSX from 'xlsx';
 import ReadExcel from './ReadExcel';
+import { onDownload } from './download';
+// const sheetHead= {header: ['账号', '使用人', '等级']}
+// const sheetHead = { header: 
+const header = [
+  '下单人',
+  '联系电话',
+  '收货人',
+  '团长备注',
+  '团员备注',
+  '地址',
+];
+const commodityCol = [];
+let sheetData = [];
+const address = ['弄号（如168）', '楼号（如10）', '房号（如606）']
 
 const Index = () => {
-  console.log('更新')
+  const onChangeData = (values) => {
+    const v = values.flat(1);
+    formatDataToDownloda(v);
+  }
+  const formatDataToDownloda = (values) => {
+    let data = [];
+    values.map(itemValue => {
+      if (!commodityCol.includes(itemValue['商品'])) {
+        commodityCol.push(itemValue['商品']);
+      }
+
+      // itemValue['地址'] = `${itemValue[address[0]]}-${itemValue[address[1]]}-${itemValue[address[2]]}`;
+      itemValue['地址'] = formatAddress(itemValue); // `${itemValue[address[0]]}-${itemValue[address[1]]}-${itemValue[address[2]]}`;
+      const obj = {};
+      header.map(key => {
+        obj[key] = itemValue[key];
+      })
+      commodityCol.map(key => {
+        if (key === itemValue['商品']) {
+          obj[key] = itemValue['数量'];
+        }
+      })
+      data.push(obj)
+    })
+    sheetData = data.sort((i, j) => { return i['地址'] - j['地址'] ? 1: -1})
+  }
+  const onClickDown = () => {
+    onDownload({ sheetHead: { header }, sheetData })
+  }
+  const regexp1 = /[0-9]/g;
+  const formatAddress = (obj) => {
+    const c = address.map(i => {
+      let a = obj[i].match(regexp1) || [obj[i]];
+      a = a.join('');
+      if (a < 10) {
+        a = '0' + a;
+      }
+      return a
+    }).join('-');
+    return c;
+  }
+
+
+
   return <div>
-    <ReadExcel />
+    <ReadExcel onChangeData={onChangeData} />
+    <Button onClick={onClickDown}>下载模版</Button>
   </div>
-} 
+}
 export default Index;
